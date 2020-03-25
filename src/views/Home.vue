31ccaa54666a030a3463f89d015d205f3e3108af
Dxxxxxxww <gotu2018@163.com>
2020-03-25 22:14:32 +0800

    提交 阻止移动端页面弹出系统键盘 by dxw

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="home">
-    <img alt="snapshoot enter" src="../assets/logo.png" @click="handleSnapshootEnter"/>
+    <img
+      alt="snapshoot enter"
+      src="../assets/logo.png"
+      @click="handleSnapshootEnter"
+    />
     <router-view name="Snapshoot"></router-view>
     <router-view name="HelloWorld"></router-view>
+    <input
+      ref="input"
+      v-model="inputData"
+      type="number"
+      @focus="focus"
+      autofocus
+    />
   </div>
 </template>
 
@@ -12,12 +23,23 @@ export default {
   props: {
     query: String
   },
+  data() {
+    return {
+      inputData: '123'
+    }
+  },
   mounted() {
     console.log('q', this.query)
+    this.$refs.input.focus()
   },
   methods: {
     handleSnapshootEnter() {
       this.$router.push('/snapshoot')
+    },
+    focus(e) {
+      // document.activeElement.blur()
+      e.preventDefault()
+      console.log(e)
     }
   }
 }
